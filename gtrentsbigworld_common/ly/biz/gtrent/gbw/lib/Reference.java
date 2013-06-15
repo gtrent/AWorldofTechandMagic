a9f12864490b088b98d17d8613cfbb6c4ff1bbfb
gtrent <gtrent9@gmail.com>
2013-06-15 01:18:40 -0500

    Just got some basic stuff together

---
+++ gtrentsbigworld_common/ly/biz/gtrent/gbw/lib/Reference.java
@@ -0,0 +1,8 @@
+package ly.biz.gtrent.gbw.lib;
+
+public class Reference {
+
+    public static final String MOD_ID = "GBW";
+    public static final String MOD_NAME = "Gtrent's Big World";
+    public static final String VERSION = "0.0.1";
+}
