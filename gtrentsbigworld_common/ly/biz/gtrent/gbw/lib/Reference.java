35bd58cc2a173e96d058a212b2217e588e9f1c6f
gtrent <gtrent9@gmail.com>
2013-07-14 19:56:45 -0500

    Update Reference.java

--- gtrentsbigworld_common/ly/biz/gtrent/gbw/lib/Reference.java
+++ gtrentsbigworld_common/ly/biz/gtrent/gbw/lib/Reference.java
@@ -2,10 +2,10 @@ package ly.biz.gtrent.gbw.lib;
 
 public class Reference {
 
-    public static final String MOD_ID = "GBW";
+    public static final String MOD_ID = "WTM";
     public static final String MOD_NAME = "A World of Tech & Magic";
     public static final String VERSION = "0.0.1";
-    public static final String CLIENT_CHANNEL1 = "GBWGeneral"; 
+    public static final String CLIENT_CHANNEL1 = "WTMGeneral"; 
     public static final String INSTANCE = MOD_ID;
     public static final String DEPENDENCIES = "";
 }
